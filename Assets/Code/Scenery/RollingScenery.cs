dd99c51d7fa580c1b0191c0b32858df801fec12c
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2021-09-06 16:46:41 +0300

    Project creation
    -Placeholders for background graphics
    -Simple scripts for moving backgrounds

---
+++ Assets/Code/Scenery/RollingScenery.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Owls.Scenery
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class RollingScenery : MonoBehaviour
+	{
+		private SpriteRenderer _spriteRenderer;
+		private float _speed;
+
+		private void Update()
+		{
+			if (_speed > 0)
+			{
+
+			}
+		}
+
+		public void Init(SceneryTexture sceneryTexture, Transform parent)
+		{
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_speed = sceneryTexture.rollSpeed;
+			_spriteRenderer.sortingOrder = sceneryTexture.layerOrder;
+			var shader = Shader.Find("Standard");
+			_spriteRenderer.material = new Material(shader) {mainTexture = sceneryTexture.texture };
+			transform.parent = parent;
+		}
+	}
+
+}
---
+++ Assets/Code/Scenery/SceneryController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Owls.Scenery
+{
+	[System.Serializable]
+	public class SceneryTexture
+	{
+		public Texture texture;
+		public float rollSpeed;
+		public int layerOrder;
+	}
+
+	public class SceneryController : MonoBehaviour
+	{
+		[SerializeField]
+		private List<SceneryTexture> sprites;
+
+		private void Awake()
+		{
+			CreateScenery();
+		}
+
+		private void OnValidate()
+		{
+			CreateScenery();
+		}
+
+		private void CreateScenery()
+		{
+			foreach (Transform child in transform)
+			{
+				StartCoroutine(DestroyChild(child.gameObject));
+			}
+
+			foreach (var s in sprites)
+			{
+				var go = new GameObject(s.texture.name, typeof(RollingScenery));
+				go.GetComponent<RollingScenery>().Init(s, transform);
+			}
+		}
+
+		private IEnumerator DestroyChild(GameObject go)
+		{
+			yield return new WaitForEndOfFrame();
+			DestroyImmediate(go);
+		}
+	}
+}
