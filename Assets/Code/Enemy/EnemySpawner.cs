c39427c47039f8e98cec520f2ed47d44c67c2b46
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2021-11-12 11:48:21 +0200

    Automatic enemy spawning

--- Assets/Code/Enemy/EnemySpawner.cs
+++ Assets/Code/Enemy/EnemySpawner.cs
@@ -6,7 +6,16 @@ namespace Owls.Enemy
 {
     public class EnemySpawner : MonoBehaviour
     {
-        private EdgeCollider2D _edge;
+		[SerializeField, Range(1f, 5f)]
+		private float spawnFrequency = 1.5f, spawnPreWait = 3f;
+
+		[SerializeField, Range(0.33f, 0.95f)]
+		private float spawnRandomizer = 0.5f;
+		
+		private EdgeCollider2D _edge;
+		private float _spawnTimer = 0f;
+		private float _targetTime = 0f;
+		private bool _spawnEnabled = false;
 
 	    public List<Enemy> enemies = new List<Enemy>();
 
@@ -15,12 +24,24 @@ namespace Owls.Enemy
 			_edge = GetComponent<EdgeCollider2D>();
 		}
 
+		private void Start()
+		{
+			Invoke(nameof(EnableSpawning), spawnPreWait);
+			_targetTime = RandomTime();
+		}
+
 		// Update is called once per frame
 		void Update()
         {
-            if (Input.GetKeyDown(KeyCode.S))
+			if (!_spawnEnabled) { return; }
+
+			_spawnTimer += Time.deltaTime;
+
+			if (_spawnTimer > _targetTime)
 			{
 				Spawn(enemies[Random.Range(0, enemies.Count)]);
+				_targetTime = RandomTime();
+				_spawnTimer = 0;
 			}
         }
 
@@ -37,6 +58,20 @@ namespace Owls.Enemy
 				Random.Range(bounds.min.x, bounds.max.x),
 				Random.Range(bounds.min.y, bounds.max.y)
 			);
+		}
+
+		private float RandomTime()
+		{
+			float variation = Mathf.Abs(spawnFrequency * Random.Range(-spawnRandomizer, spawnRandomizer));
+			float max = spawnFrequency + variation;
+			float t = Random.Range(0, max);
+			Debug.Log("Random time is " + t);
+			return t;
+		}
+
+		private void EnableSpawning()
+		{
+			_spawnEnabled = true;
 		}
 	}
 }
