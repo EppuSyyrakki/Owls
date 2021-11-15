d4bd29ec5bd42f478dba4549d414eb86d86e95fc
Eppu <eppu.syyrakki@tuni.fi>
2021-11-16 00:22:50 +0200

    Spell Initialization

--- Assets/Code/Player/Badger.cs
+++ Assets/Code/Player/Badger.cs
@@ -3,42 +3,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Owls.Enemy;
+using Owls.Spells;
 
 namespace Owls.Player
 {
-	public class Badger : MonoBehaviour
+	public class Badger : MonoBehaviour, ITargetable
 	{
 		private const string ANIM_TAKEDAMAGE = "TakeDamage";
 		private const string ANIM_DIE = "Die";
-		private const string ANIM_SHIELD = "Shield";
 
-		private float health = 1f;
+		private float _health = 1f;
 		private Animator _animator;
 
-		public Action<float> healthChanged;
+		public bool IsAlive { get; private set; } = true;
+
+		/// <summary>
+		/// Delegate that is called when Badger health changes. First parameter is
+		/// the amount changed and the second is remaining health.
+		/// </summary>
+		public Action<float, float> healthChanged;
 
 		private void Awake()
 		{
 			_animator = GetComponent<Animator>();
 		}
 
+		public void TargetedBySpell(float amount)
+		{
+			if (amount < 0) { TakeDamage(Mathf.Abs(amount)); }
+			else if (amount > 0) { HealDamage(Mathf.Abs(amount)); }
+		}
+
 		public void TakeDamage(float amount)
 		{
-			health = Mathf.Clamp01(health - amount);
-			Debug.Log(string.Format("Badger took {0} damage. {1} remaining.", 
-				amount, health));
+			if (!IsAlive) { return; }
 
-			if (health == 0) 
-			{ 
-				_animator.SetTrigger(ANIM_DIE);
-				Debug.Log("Badger died.");
-			}
-			else 
-			{
-				_animator.SetTrigger(ANIM_TAKEDAMAGE); 
-			}
+			_health = Mathf.Clamp01(_health - amount);
 
-			healthChanged?.Invoke(health);
+			if (_health == 0) { Die(); }
+			else { _animator.SetTrigger(ANIM_TAKEDAMAGE); }
+
+			healthChanged?.Invoke(amount, _health);
+		}
+
+		private void HealDamage(float amount)
+		{
+			if (!IsAlive) { return; }
+
+			_health = Mathf.Clamp01(_health + amount);
+			healthChanged?.Invoke(amount, _health);
+		}
+
+		private void Die()
+		{
+			_animator.SetTrigger(ANIM_DIE); 
+			IsAlive = false;
 		}
 	}
 }
--- Assets/Code/Player/WorldTouchPointer.cs
+++ Assets/Code/Player/WorldTouchPointer.cs
@@ -16,16 +16,19 @@ namespace Owls.Player
 		[SerializeField]
 		private LayerMask worldPlane;
 
+		[SerializeField, Tooltip("Max deviation from a straight line allowed to cast basic spell")]
+		private float swipeAngleMax = 15f;
+
 		private Camera _cam;
 		private TrailRenderer _activeTrail = null;
 		private List<Vector2> _stroke = null;
-		private GameObject _oldTrails;
+		private Transform _oldTrails;
 
 		private void Awake()
 		{
 			particle.Stop();
 			_cam = Camera.main;
-			_oldTrails = new GameObject("Old trails");
+			_oldTrails = new GameObject("Old trails").transform;
 		}
 
 		private void Update()
@@ -37,14 +40,13 @@ namespace Owls.Player
 				if (touch.phase == TouchPhase.Began) { BeginTouch(touch); }
 				else if (touch.phase == TouchPhase.Moved) { ContinueTouch(touch); }
 				else if (touch.phase == TouchPhase.Ended) { CompleteTouch(touch); }
-				else if (touch.phase == TouchPhase.Stationary) { CancelTouch(touch); }
+				else if (touch.phase == TouchPhase.Stationary) { CancelTouch(); }
 			}
 		}
 
 		private void BeginTouch(Touch touch)
 		{
 			_stroke = new List<Vector2>();
-			_stroke.Add(touch.position);
 			Move(touch.position);
 			particle.Play();
 			_activeTrail = Instantiate(trailPrefab, transform.position, Quaternion.identity, transform);
@@ -57,19 +59,21 @@ namespace Owls.Player
 
 		private void CompleteTouch(Touch touch)
 		{
-			// TODO: send _stroke list to glyph input
-			StopActiveTrail();
 			Move(touch.position);
+
+			if (CompareStrokeAngles()) 
+			{
+				// Do the lightning Spell
+			}
+			//else if (glyphRrecognition.match(_stroke, out var glyph) 
+			//		var spell = spellLookUp(glyph)
+			//		instantiate(spell)
+
+			StopActiveTrail();
 			particle.Stop();
 		}
 
-		private void StopActiveTrail()
-		{
-			_activeTrail.transform.parent = _oldTrails.transform;
-			_activeTrail = null;
-		}
-
-		private void CancelTouch(Touch touch)
+		private void CancelTouch()
 		{
 			StopActiveTrail();
 			particle.Stop();
@@ -77,12 +81,33 @@ namespace Owls.Player
 
 		private void Move(Vector2 screenPos)
 		{
+			
 			Ray ray = _cam.ScreenPointToRay(screenPos);
 			var hit = Physics2D.Raycast(ray.origin, ray.direction * 20f, 40f, worldPlane);
-			
+
 			if (hit.collider == null) { return; }
 
+			_stroke.Add(hit.point);
 			transform.position = hit.point;
+		}
+
+		private bool CompareStrokeAngles()
+		{
+			for (int i = 0; i < _stroke.Count - 1; i++)
+			{
+				var angle = Vector2.Angle(_stroke[i], _stroke[i + 1]);
+				if (angle > swipeAngleMax) { return false; }
+			}
+
+			return true;
+		}
+
+		private void StopActiveTrail()
+		{
+			if (_activeTrail == null) { return; }
+
+			_activeTrail.transform.parent = _oldTrails;
+			_activeTrail = null;
 		}
 	}
 }
