902547210989522500b6e613e16e4f00e13f2850
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2021-11-26 17:26:07 +0200

    Basic Timewarp spell implemented

---
+++ Assets/Code/Spells/Timewarp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Owls.Spells
+{
+	public class Timewarp : Spell
+	{
+		[SerializeField, Range(0.1f, 0.75f)]
+		private float slowedTime = 0.33f;
+
+		private bool _speedUpTriggered = false;
+
+		/// <summary>
+		/// Can be used to fetch references to class members or initialize them.
+		/// </summary>
+		public override void Init(List<Vector2> stroke)
+		{
+			// base.init should be run before doing Init logic for this class.
+			base.Init(stroke);
+		}
+
+		private void Start()
+		{
+			StartCoroutine(AdjustTime(1, slowedTime, info.lifeTime * 0.2f));
+		}
+
+		private void OnDisable()
+		{
+			Time.timeScale = 1;
+		}
+
+		/// <summary>
+		/// This gets called by the caster every frame.
+		/// </summary>
+		public override void Update()
+		{
+			// Advance a timer in base. Destroys gameObject if lifetime passed. 
+			// Base.Execute can be called  before or after this spell's logic.
+			base.Update();
+
+			if (!_speedUpTriggered && timeLived > 0.75f * info.lifeTime)
+			{
+				StartCoroutine(AdjustTime(slowedTime, 1, 0.2f * info.lifeTime));
+				_speedUpTriggered = true;
+			}
+		}
+
+		private IEnumerator AdjustTime(float from, float to, float duration)
+		{
+			float time = 0;
+
+			while (time < duration)
+			{
+				Time.timeScale = Mathf.Lerp(from, to, time / duration);
+				yield return new WaitForEndOfFrame();
+				time += Time.deltaTime;
+			}
+		}
+	}
+}
