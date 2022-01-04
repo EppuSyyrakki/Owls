f245918f199acc09adc1c406a5364ee4e62c7a90
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2022-01-04 12:46:00 +0200

    Spellbook general help panel

---
+++ Assets/Code/GUI/SpellBookHelp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Owls.GUI
+{
+	public class SpellBookHelp : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject helpScreen = null;
+
+		[SerializeField]
+		private CanvasGroup disableGroup = null;
+
+		private void Update()
+		{
+			if (!disableGroup.interactable && Input.GetKeyDown(KeyCode.Escape))
+			{
+				HideHelp();
+			}
+		}
+
+		public void DisplayHelp()
+		{
+			helpScreen.SetActive(true);
+			disableGroup.interactable = false;
+			disableGroup.alpha = 0.25f;
+		}
+
+		public void HideHelp()
+		{
+			helpScreen.SetActive(false);
+			disableGroup.interactable = true;
+			disableGroup.alpha = 1f;
+		}
+	}
+}
