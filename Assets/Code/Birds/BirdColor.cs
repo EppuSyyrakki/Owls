e556d67f3c8fb0975c17a36d9a444bc80a921f8a
Hannu Timonen <hannu.timonen@tuni.fi>
2022-02-09 14:24:29 +0200

    bird color script added

---
+++ Assets/Code/Birds/BirdColor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdColor : MonoBehaviour
+{
+
+    [SerializeField] private Color[] colors = new Color[5]; // Open color list to inspector
+
+    // Color birds randomly from array when they appear.
+    void Awake()
+    {
+        int random = Random.Range(0, colors.Length);         
+
+        Color birdColor = colors[random]; // Choose bird color at random
+        GetComponent<SpriteRenderer>().color = birdColor;  // Set chosen random color 
+
+    }
+
+}   
