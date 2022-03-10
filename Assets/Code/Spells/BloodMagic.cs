e3289b07e05624e58ca09d92d82e47291a8fe5f5
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2022-03-10 14:44:58 +0200

    Blood Magic health bug fix

--- Assets/Code/Spells/BloodMagic.cs
+++ Assets/Code/Spells/BloodMagic.cs
@@ -24,7 +24,7 @@ namespace Owls.Spells
 			}
 
 			badger.AddMana(info.effectAmount);
-			badger.TakeDamage(-info.effectAmount);
+			badger.TakeDamage(info.effectAmount);
 			SpawnHitEffect();
 		}
 
